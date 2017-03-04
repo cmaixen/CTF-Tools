7f0db1528439371ede1f3a786761fc93f359bcec
Yannick Merckx <dj-d@telenet.be>
2017-03-04 11:13:35 +0100

    Create decode_ceasar_cipher.py

---
+++ cryptography/decode_ceasar_cipher.py
@@ -0,0 +1,34 @@
+import sys
+from guess_language import guess_language
+
+def decode_ceasar_cipher(argv):
+
+    cipher=argv[0].lower()
+    keyspace = []
+    print('\nAll Options:')
+    print('------------\n')
+    for x in range(26):
+        result = []
+        for i in cipher:
+           if i == " " or i == "." or i.isdigit():
+             result.append(i) 
+           else:
+             result.append(str(chr(97 + (((ord(i) - 97) - x) % 26 ))))
+
+        print("".join(result))
+        keyspace.append("".join(result))
+    
+
+    for j in keyspace:
+        lang = guess_language(j)
+        if lang == 'en': 
+
+            print("\nFound Valid Decription: " + j + '\n')
+
+
+ 
+
+if __name__ == '__main__':
+    if(len(sys.argv) == 1):
+        print("Usage: python decode_ceasar_cipher.py ciphertext")
+    decode_ceasar_cipher(sys.argv[1:])
